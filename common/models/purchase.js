87aaa8b99e4d0af3319b415da7cc65b18f722558
croussy <clroussy@laposte.net>
2016-06-15 15:01:56 +0200

    add trigger purchase

--- common/models/purchase.js
+++ common/models/purchase.js
@@ -1,3 +1,13 @@
 module.exports = function(Purchase) {
 
+  Purchase.observe('after save', function(ctx, next) {
+    var GoalCategory = ctx.Model.app.models.GoalCategory;
+    GoalCategory.find({where: {id: ctx.instance.goalCategoryId}}, function(err, data) {
+      for(var i = 0; i < data.length; i++) {
+        data[i].amountCateg = data[i].amountCateg + ctx.instance.amountPurchase;
+        data[i].save();
+      }
+    });
+    next();
+  });
 };
