537056e1f82b61b589cec5749a20831dec44274b
croussy <clroussy@laposte.net>
2016-06-17 22:33:07 +0200

    update purchase's model && trigger purchase

--- common/models/purchase.js
+++ common/models/purchase.js
@@ -1,13 +1,15 @@
 module.exports = function(Purchase) {
 
   Purchase.observe('after save', function(ctx, next) {
+    console.log(ctx.instance);
     var GoalCategory = ctx.Model.app.models.GoalCategory;
-    GoalCategory.find({where: {id: ctx.instance.goalCategoryId}}, function(err, data) {
-      for(var i = 0; i < data.length; i++) {
-        data[i].amountCateg = data[i].amountCateg + ctx.instance.amountPurchase;
-        data[i].save();
-      }
-    });
+      GoalCategory.find({where: {id: ctx.instance.goalCategoryId}}, function(err, data) {
+        for(var i = 0; i < data.length; i++) {
+          console.log(data[i].amountCateg);
+          data[i].amountCateg = data[i].amountCateg + ctx.instance.amountPurchase;
+          data[i].save();
+        }
+      });
     next();
   });
 };
