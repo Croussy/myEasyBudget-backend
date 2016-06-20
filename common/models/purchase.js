da97780922e6c201949c3ca6e664c5fd1a06ab5a
croussy <clroussy@laposte.net>
2016-06-20 15:08:53 +0200

    update trigger after save purchase

--- common/models/purchase.js
+++ common/models/purchase.js
@@ -1,15 +1,18 @@
 module.exports = function(Purchase) {
 
   Purchase.observe('after save', function(ctx, next) {
-    console.log(ctx.instance);
     var GoalCategory = ctx.Model.app.models.GoalCategory;
-      GoalCategory.find({where: {id: ctx.instance.goalCategoryId}}, function(err, data) {
-        for(var i = 0; i < data.length; i++) {
-          console.log(data[i].amountCateg);
-          data[i].amountCateg = data[i].amountCateg + ctx.instance.amountPurchase;
-          data[i].save();
-        }
-      });
+    var Budget = ctx.Model.app.models.Budget;
+
+    GoalCategory.find({where: {id: ctx.instance.goalCategoryId}}, function(err, data) {
+      data[0].amountCateg += ctx.instance.amountPurchase;
+      data[0].save();
+    });
+
+    Budget.find({where: {id: ctx.instance.budgetId}}, function(err, data) {
+      data[0].amount -=  ctx.instance.amountPurchase;
+      data[0].save();
+    });
     next();
   });
 };
