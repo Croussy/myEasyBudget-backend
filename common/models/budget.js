4ebc6254767db7841ca15766e75a8f181b969e99
croussy <clroussy@laposte.net>
2016-06-22 15:26:22 +0200

    add update amount budget

--- common/models/budget.js
+++ common/models/budget.js
@@ -1,3 +1,20 @@
 module.exports = function(Budget) {
+  Budget.updateAmount = function(id, amount, cb) {
+    Budget.find({where: {id: id}}, function(err, data) {
+      data[0].amount += amount;
+      data[0].save();
+    });
+    cb(null, 'Done !');
+  };
 
+  Budget.remoteMethod(
+    'updateAmount',
+    {
+      accepts: [
+        {arg: 'id', type: 'string', required: true},
+        {arg: 'amount', type: 'number', required: true}
+      ] ,
+      returns: {arg: 'greeting', type: 'string'}
+    }
+  );
 };
